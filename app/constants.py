6ffdae2124a52d77b085a12e255992619d3c411c
Fred Bane <fwbane@gmail.com>
2024-01-07 08:48:35 -0800

    Adding dev and test DBs

--- app/constants.py
+++ app/constants.py
@@ -1,7 +1,10 @@
 import os
 
 TIME_FORMAT="%Y-%m-%d-%H%M"
-DB_PATH=os.path.join("db","things.db")
+DEV_DB_PATH=os.path.join("db","dev.db")
+PROD_DB_PATH=os.path.join("db","prod.db")
+TEST_DB_PATH=os.path.join("db","test.db")
+DB_PATH=DEV_DB_PATH # Change for production, testing
 ONE_MONTH=2629728 # 86400 * 365.24 / 12  ??
 ONE_WEEK=604800
 TWO_DAYS=172800
